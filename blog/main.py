ebc572274b5fa6b0701a3f43b3e7b59cbf244482
Anik Chand <anikchand461@gmail.com>
2025-04-21 16:55:43 +0530

    created a homepage for users

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from . import models
 from .database import engine
 from .routers import blog, user, authentication
@@ -7,9 +8,23 @@ app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-@app.get("/")
+html_content = """
+<html>
+    <head>
+        <title>FastAPI Demo</title>
+        
+    </head>
+    <body>
+        <h1>Welcome to My FastAPI Demo API 🚀</h1>
+        <p>Click below to view the interactive API docs:</p>
+        <a href="/docs" target="_blank" style="font-size: 25px; color: #2196F3; font-weight: bold;">Go to Swagger Docs</a>
+    </body>
+</html>
+"""
+
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"message": "Welcome to my demo-api"}
+    return html_content
 
 app.include_router(blog.router)
 app.include_router(user.router)
